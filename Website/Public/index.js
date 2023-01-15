103cb23bb34ebd156b9d22d96a806f4a0945eace
dstremur <76773187+dstremur@users.noreply.github.com>
2023-01-15 09:39:15 +0100

    Running Website Prototype

---
+++ Website/Public/index.js
@@ -0,0 +1,79 @@
+
+
+async function createImage(){
+
+
+const model = await tf.loadLayersModel('DCGANJavaScript/model.json')
+
+console.log(model.summary());
+
+const randomVector = tf.randomNormal([1,128]);
+    //console.log(randomVector);
+
+    img = await model.predict(randomVector);
+    console.log(img)
+    
+    scalar = tf.scalar(127.5)
+    // remove rank of tensor
+    img = img.squeeze();
+    
+
+    console.log(img);
+    img = img.mul(scalar);
+    img = img.add(scalar);
+    
+    console.log(img);
+    img.print()
+    img = img.div(255)
+
+    img = tf.image.resizeBilinear(img,[512,512])
+
+    canvas = document.createElement('canvas');
+    Bild = await tf.browser.toPixels(img,canvas);
+
+    data = canvas.toDataURL();
+
+    
+    document.getElementById("img").src = data;
+    
+    
+    
+
+    createDownloadButton()
+
+    img.dispose();
+    scalar.dispose();
+}
+
+
+
+
+
+function createDownloadButton(){
+    var button = document.createElement("button");
+    button.className = "btn btn-secondary"
+    button.innerText = "Download"
+    buttondiv = document.getElementById("buttondiv");
+    buttondiv.appendChild(button);
+    button.addEventListener("click", DownloadImage)
+}
+
+function DownloadImage(){
+    var download = canvas.toDataURL();
+
+    var link = document.createElement("a");
+    link.attribute = "Download"
+    link.href = download; 
+    link.download = "image.png"
+
+    link.click();
+
+    console.log(download)
+    console.log("sff")
+}
+
+
+
+createImage()
+
+
