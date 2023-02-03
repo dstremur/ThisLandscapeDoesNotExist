f5a6006f5bc014fb51afa80d985c80095928135e
dstremur <diego@strebel.com>
2023-02-03 23:34:18 +0100

    Navbar finally breaks

--- Website/Public/index.js
+++ Website/Public/index.js
@@ -1,24 +1,32 @@
 
 
+
 async function createImage(){
 
 
-const model = await tf.loadLayersModel('DCGANJavaScript/model.json')
+    tf.enableProdMode();
+// Make 2 versions: 
+// 64x64: WebGL
+// 128x128: WASM
+//tf.setBackend('wasm')
 
-console.log(model.summary());
+//const model = await tf.loadLayersModel('Chollet200EpochsRMSProp256JS/model.json')
+//const model = await tf.loadLayersModel('Chollet200EpochsRMSPROP128JS/model.json')
+const model = await tf.loadLayersModel('DCGANJavaScript/model.json')
+//console.log(model.summary());
 
 const randomVector = tf.randomNormal([1,128]);
     //console.log(randomVector);
 
     img = await model.predict(randomVector);
-    console.log(img)
+    //console.log(img)
     
     scalar = tf.scalar(127.5)
     // remove rank of tensor
     img = img.squeeze();
     
 
-    console.log(img);
+    //console.log(img);
     img = img.mul(scalar);
     img = img.add(scalar);
     
@@ -26,10 +34,12 @@ const randomVector = tf.randomNormal([1,128]);
     img.print()
     img = img.div(255)
 
-    img = tf.image.resizeBilinear(img,[512,512])
+    img = tf.image.resizeBilinear(img,[256,256])
 
     canvas = document.createElement('canvas');
     Bild = await tf.browser.toPixels(img,canvas);
+
+    //console.log(Bild)
 
     data = canvas.toDataURL();
 
@@ -37,14 +47,19 @@ const randomVector = tf.randomNormal([1,128]);
     document.getElementById("img").src = data;
     
     
-    
+    console.log(tf.memory());
 
-    createDownloadButton()
+    createDownloadButton();
 
     img.dispose();
     scalar.dispose();
-}
+    model.dispose();
+    randomVector.dispose();
+    tf.disposeVariables();
+    console.log(tf.memory());
+    console.log(tf.engine());
 
+}
 
 
 
@@ -73,7 +88,8 @@ function DownloadImage(){
 }
 
 
+createImage();
 
-createImage()
+
 
 
