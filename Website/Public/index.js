68e3385dd293307e652e8a5fac52179ca13bf13b
dstremur <76773187+dstremur@users.noreply.github.com>
2023-02-12 19:16:45 +0100

    Cleanup remove oversize files

--- Website/Public/index.js
+++ Website/Public/index.js
@@ -10,44 +10,52 @@ async function createImage(){
 // 128x128: WASM
 //tf.setBackend('wasm')
 
-//const model = await tf.loadLayersModel('Chollet200EpochsRMSProp256JS/model.json')
-//const model = await tf.loadLayersModel('Chollet200EpochsRMSPROP128JS/model.json')
-const model = await tf.loadLayersModel('DCGANJavaScript/model.json')
+
+const model = await tf.loadLayersModel('DCGAN128/model.json')
+
 //console.log(model.summary());
 
-const randomVector = tf.randomNormal([1,128]);
+const randomVector = tf.randomNormal([1,100]);
     //console.log(randomVector);
 
     img = await model.predict(randomVector);
-    //console.log(img)
+
+
+    //img.print() 
     
     scalar = tf.scalar(127.5)
     // remove rank of tensor
     img = img.squeeze();
-    
+
 
     //console.log(img);
     img = img.mul(scalar);
     img = img.add(scalar);
     
-    console.log(img);
-    img.print()
+
+    
+    //img.print()
     img = img.div(255)
 
-    img = tf.image.resizeBilinear(img,[256,256])
+
+    //img = tf.image.resizeNearestNeighbor(img,[512,512])
+    img = tf.image.resizeBilinear(img,[512,512])
 
     canvas = document.createElement('canvas');
     Bild = await tf.browser.toPixels(img,canvas);
+
 
     //console.log(Bild)
 
     data = canvas.toDataURL();
 
+
+
     
     document.getElementById("img").src = data;
     
     
-    console.log(tf.memory());
+    //console.log(tf.memory());
 
     createDownloadButton();
 
@@ -56,8 +64,8 @@ const randomVector = tf.randomNormal([1,128]);
     model.dispose();
     randomVector.dispose();
     tf.disposeVariables();
-    console.log(tf.memory());
-    console.log(tf.engine());
+    // console.log(tf.memory());
+    // console.log(tf.engine());
 
 }
 
